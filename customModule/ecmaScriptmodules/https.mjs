91841cda3ab7ab22abd1f11fa78cd8145c072a41
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-24 15:11:05 +0300

    .

--- customModule/ecmaScriptmodules/https.mjs
+++ customModule/ecmaScriptmodules/https.mjs
@@ -1,5 +1,5 @@
-import { send } from './request';
-import { read } from './response';
+import { send } from './request.mjs';
+import { read } from './response.mjs';
 
 function makeRequest(url, data) {
   send(url, data);
