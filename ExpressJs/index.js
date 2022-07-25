e83958fe7dd190a22a6cfdb58276b53d33007722
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-25 23:56:37 +0300

    .

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express(); // create server
 const PORT = 3001;
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
 app.listen(PORT, () => {
   console.log('server is on');
 });
