c1613275b53517dc93eb98ba8e06f20b3dcaf531
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-25 23:58:03 +0300

    .

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+app.get('/messages', (req, res) => {
+  res.send('first message');
+});
+
+app.post('/messages', (req, res) => {
+  console.log('opdating messages');
+});
+
 app.listen(PORT, () => {
   console.log('server is on');
 });
