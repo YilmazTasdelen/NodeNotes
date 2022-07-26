4f32ecd071e3a0ad3e407a807217b0d15d7ba301
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-26 14:34:28 +0300

    .

--- ExpressJs/server.js
+++ ExpressJs/server.js
@@ -3,6 +3,31 @@ const express = require('express');
 const app = express(); // create server
 const PORT = 3001;
 
+const friends = [
+  {
+    id: 1,
+    name: 'test',
+  },
+  {
+    id: 2,
+    name: 'test2',
+  },
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friends);
+  } else {
+    res.status(404).json({ error: 'friend does not exist' });
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('hello world');
 });
