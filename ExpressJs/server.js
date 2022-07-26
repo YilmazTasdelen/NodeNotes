8c6eee1a4ef64c2cef28ba7c78378e83a4bfd937
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-26 16:51:31 +0300

    .

--- ExpressJs/server.js
+++ ExpressJs/server.js
@@ -1,22 +1,13 @@
 const express = require('express');
+const messagesController = require('./controllers/messages.controller');
+const friendsController = require('./controllers/friends.controller');
 
 const app = express(); // create server
 const PORT = 3001;
 
-const friends = [
-  {
-    id: 1,
-    name: 'test',
-  },
-  {
-    id: 2,
-    name: 'test2',
-  },
-];
-
 app.use((req, res, next) => {
   const start = Date.now();
-  next(); //passes the action the right route
+  next(); //passes the action the right route handler
   //after action execute response return the middle ware
   const delta = Date.now() - start;
   console.log(`${req.method} - ${req.url} ${delta}ms`);
@@ -24,44 +15,50 @@ app.use((req, res, next) => {
 
 app.use(express.json()); // apply the json parser middleware
 
-app.get('/friends', (req, res) => {
-  res.json(friends);
-});
+// app.get('/friends', (req, res) => {
+//   res.json(friends);
+// });
 
-app.post('/friends', (req, res) => {
-  if (!req.body.name) {
-    res.status(400).json({ error: 'missing friend name' });
-  }
-  const newFriend = {
-    //request.body doesnt exist unless we parse json
-    name: req.body.name,
-    id: friends.length,
-  };
-  friends.push(newFriend);
-  res.json(newFriend);
-});
+app.get('/friends', friendsController.getFriends);
 
-app.get('/friends:friendId', (req, res) => {
-  const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
-  if (friend) {
-    res.status(200).json(friends);
-  } else {
-    res.status(404).json({ error: 'friend does not exist' });
-  }
-});
+// app.post('/friends', (req, res) => {
+//   if (!req.body.name) {
+//     res.status(400).json({ error: 'missing friend name' });
+//   }
+//   const newFriend = {
+//     //request.body doesnt exist unless we parse json
+//     name: req.body.name,
+//     id: friends.length,
+//   };
+//   friends.push(newFriend);
+//   res.json(newFriend);
+// });
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.post('/friends', friendsController.postFriend);
 
-app.get('/messages', (req, res) => {
-  res.send('first message');
-});
+// app.get('/friends:friendId', (req, res) => {
+//   const friendId = Number(req.params.friendId);
+//   const friend = friends[friendId];
+//   if (friend) {
+//     res.status(200).json(friends);
+//   } else {
+//     res.status(404).json({ error: 'friend does not exist' });
+//   }
+// });
 
-app.post('/messages', (req, res) => {
-  console.log('opdating messages');
-});
+app.get('/friends:friendId', friendsController.getFriend);
+
+// app.get('/messages', (req, res) => {
+//   res.send('first message');
+// });
+
+app.get('/messages', messagesController.getMessages);
+
+// app.post('/messages', (req, res) => {
+//   console.log('opdating messages');
+// });
+
+app.post('/messages', messagesController.postMessages);
 
 app.listen(PORT, () => {
   console.log('server is on');
