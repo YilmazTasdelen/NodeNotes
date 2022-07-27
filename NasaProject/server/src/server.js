1894519dd775a44da9b8e9ca2d10e6fb829df11f
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-27 20:13:16 +0300

    .

--- NasaProject/server/src/server.js
+++ NasaProject/server/src/server.js
@@ -1,2 +1,12 @@
+const http = require('http');
+
+const app = require('./app');
+
 const PORT = process.env.PORT || 8000;
 console.log(PORT);
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
