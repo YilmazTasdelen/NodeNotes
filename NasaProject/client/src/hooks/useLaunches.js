3bde8447d67c60f88f932ace7fdfaefec8635a0f
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-07-31 23:17:09 +0300

    .

--- NasaProject/client/src/hooks/useLaunches.js
+++ NasaProject/client/src/hooks/useLaunches.js
@@ -35,7 +35,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
     });
 
     // TODO: Set success based on response.
-    const success = false;
+    const success = response.ok;
     if (success) {
       getLaunches();
       setTimeout(() => {
